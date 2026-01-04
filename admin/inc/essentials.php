d72c97c8b71d6c37e69343932167253738bd9550
YashKamdar2004 <yashkamdar872@gmail.com>
2026-01-05 00:24:32 +0530

    files added and updated

--- admin/inc/essentials.php
+++ admin/inc/essentials.php
@@ -1,5 +1,23 @@
 <?php 
 
+function adminLogin(){
+    session_start();
+    if(!(isset($_SESSION['adminLogin']) && $_SESSION['adminLogin']==true)){
+        echo "<script>
+            window.location.href='index.php';
+        </script>";
+    }
+    session_regenerate_id(true);
+}
+
+
+function redirect($url){
+    echo "<script>
+        window.location.href='$url';
+    </script>";
+}
+
+
 function alert($type,$msg){
 
     $bs_class = ($type == "success") ? "alert-success" : "alert-danger";
