35d4738d076609cc4a951e22f58da496f92384e1
YashKamdar2004 <yashkamdar872@gmail.com>
2026-01-09 17:29:52 +0530

    changes made in settings.php and settings_crud

--- admin/settings.php
+++ admin/settings.php
@@ -226,6 +226,7 @@
         let site_title_inp = document.getElementById('site_title_inp');
         let site_about_inp = document.getElementById('site_about_inp');
 
+        let contacts_s_form = document.getElementById('contacts_s_form');
 
         function get_general()
         {
@@ -347,6 +348,45 @@
             {
                 document.getElementById(contacts_inp_id[i]).value = data[i+1];
             }
+        }
+
+        contacts_s_form.addEventListener('submit',function(e){
+            e.preventDefault();
+            upd_contacts();
+        })
+
+        function upd_contacts()
+        {
+            let index = ['address','gmap','pn1','pn2','email','fb','insta','tw','iframe'];
+            let contacts_inp_id = ['address_inp','gmap_inp','pn1_inp','pn2_inp','email_inp','fb_inp','insta_inp','tw_inp','iframe_inp'];
+
+            let data_str = "";
+
+            for(i=0;i<index.length;i++){
+                data_str += index[i] + "=" + document.getElementById(contacts_inp_id[i]).value + '&';
+            }
+            data_str += "upd_contacts";
+              
+            let xhr = new XMLHttpRequest();
+            xhr.open("POST","ajax/settings_crud.php",true);
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+            xhr.onload = function(){
+                var myModal = document.getElementById('contacts-s'); 
+                var modal = bootstrap.Modal.getInstance(myModal);
+                modal.hide();
+                if(this.responseText == 1)
+                {
+                    alert('success','Changes saved!');
+                    get_contacts();
+                }
+                else
+                {
+                    alert('error','No changes made!');
+                }  
+            }
+
+            xhr.send(data_str);
         }
         
         window.onload = function(){
