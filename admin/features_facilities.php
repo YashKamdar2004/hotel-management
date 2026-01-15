1715dc4554beebd6e92a83f8fb85777e9e07053c
YashKamdar2004 <yashkamdar872@gmail.com>
2026-01-15 19:39:02 +0530

    new block of code added in features_facilities.php

--- admin/features_facilities.php
+++ admin/features_facilities.php
@@ -80,6 +80,8 @@
         <div class="row">
             <div class="col-lg-10 ms-auto p-4">
                 <h3 class="mb-4">FEATURES & FACILITIES</h3>
+
+                <!-- first table -->
             
                 <div class="card border-0 shadow-sm mb-4">
                     <div class="card-body">
@@ -102,6 +104,37 @@
                                 </thead>
                                 <tbody id="features-data">
                                     
+                                </tbody>
+                            </table>
+                        </div>
+
+                    </div>
+                </div>
+
+                <!-- second table -->
+
+                <div class="card border-0 shadow-sm mb-4">
+                    <div class="card-body">
+
+                      <div class="d-flex align-items-center justify-content-between mb-3">
+                            <h5 class="card-title m-0">Facilities</h5>
+                            <button type="button" class="btn btn-dark shadow-none btn-sm" data-bs-toggle="modal" data-bs-target="#facility-s">
+                                <i class="bi bi-plus-square"></i> Add
+                            </button>
+                        </div>  
+    
+                        <div class="table-responsive-md" style="height: 350px; overflow-y: scroll;">
+                            <table class="table table-hover border">
+                                <thead class="sticky-top">
+                                    <tr class="bg-dark text-light">
+                                        <th scope="col">#</th>
+                                        <th scope="col">Icon</th>
+                                        <th scope="col">Name</th>
+                                        <th scope="col">Description</th>
+                                        <th scope="col">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody id="facilities-data">
                                 </tbody>
                             </table>
                         </div>
@@ -138,12 +171,46 @@
         </div>
     </div>
 
+    <!-- Facility modal -->
+
+    <div class="modal fade" id="facility-s" data-bs-backdrop="static" data-bs-keyboard="true" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <form id="facility_s_form">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Add Facility</h5>
+                    </div>
+                    <div class="modal-body">
+                        <div class="mb-3">
+                            <label class="form-label fw-bold">Name</label>
+                            <input type="text" name="facility_name" class="form-control shadow-none" required>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label fw-bold">Icon</label>
+                            <input type="file" name="facility_icon" accept=".svg" class="form-control shadow-none" required>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Description</label>
+                            <textarea name="facility_desc" class="form-control shadow-none" rows="3"></textarea>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="reset" class="btn text-secondary shadow-none" data-bs-dismiss="modal">CANCEL</button>
+                        <button type="submit" class="btn custom-bg text-white shadow-none">SUBMIT</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+
+
     <?php require('inc/scripts.php');?>
 
 
     <script>
 
         let feature_s_form = document.getElementById('feature_s_form');
+        let facility_s_form = document.getElementById('facility_s_form');
 
         feature_s_form.addEventListener('submit',function(e){
             e.preventDefault();
@@ -210,6 +277,9 @@
                     alert('success','Feature removed!');
                     get_features();
                 }
+                else if(this.responseText == 'room_added'){
+                    alert('error','Feature is added in room!');
+                }
                 else
                 {
                     alert('error','Server down!');
@@ -218,6 +288,49 @@
 
             xhr.send('rem_feature='+val);      
         }
+
+        function add_facility(){
+            let data = new FormData();
+            data.append('name',facility_s_form.elements['facility_name'].value);
+            data.append('name',facility_s_form.elements['facility_icon'].files[0]);
+            data.append('name',facility_s_form.elements['facility_desc'].value);
+            data.append('add_facility', '');
+
+            let xhr = new XMLHttpRequest();
+            xhr.open("POST","ajax/features_facilities.php",true);
+
+            xhr.onload = function(){
+               
+                var myModal = document.getElementById('facility-s'); 
+                var modal = bootstrap.Modal.getInstance(myModal);
+                modal.hide();
+
+                if(this.responseText == 'inv_img')
+                {
+                    alert('error','Only SVG images are allowed!');
+                }
+                else if(this.responseText == 'inv_size')
+                {
+                    alert('error','Image should be less than 1MB!');
+                }
+                else if(this.responseText == 'upd_failed')
+                {
+                    alert('error','Image upload failed. Server Down!');
+                }
+                else{
+                    alert('success','New facility added');
+                    facility_s_form.reset();
+                    //get_members();
+                }
+            }
+
+            xhr.send(data);
+        }
+
+        facility_s_form.addEventListener('submit',function(e){
+            e.preventDefault();
+            add_facility();
+        });
 
     </script>
 
