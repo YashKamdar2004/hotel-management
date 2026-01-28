6f8661418657d44c6861a570ab2b70cf719f052c
YashKamdar2004 <yashkamdar872@gmail.com>
2026-01-28 13:53:00 +0530

    new file added in ajax folder rooms.php

--- admin/rooms.php
+++ admin/rooms.php
@@ -161,29 +161,54 @@
 
             let data = new FormData();
             data.append('add_room','');
-            data.append('name',feature_s_form.elements['feature_name'].value);
+            data.append('name',add_room_form.elements['name'].value);
+            data.append('area',add_room_form.elements['area'].value);
+            data.append('price',add_room_form.elements['price'].value);
+            data.append('quantity',add_room_form.elements['quantity'].value);
+            data.append('adult',add_room_form.elements['adult'].value);
+            data.append('children',add_room_form.elements['children'].value);
+            data.append('desc',add_room_form.elements['desc'].value);
 
-            /* let xhr = new XMLHttpRequest();
-            xhr.open("POST","ajax/features_facilities.php",true);
+            let features = [];
+
+            add_room_form.elements['features'].forEach(el => {
+                if(el.checked){
+                    features.push(el.value);
+                }
+            });
+
+            let facilities = [];
+
+            add_room_form.elements['facilities'].forEach(el => {
+                if(el.checked){
+                    facilities.push(el.value);
+                }
+            });
+
+            data.append('features',JSON.stringify(features));
+            data.append('facilities',JSON.stringify(facilities));
+
+            let xhr = new XMLHttpRequest();
+            xhr.open("POST","ajax/rooms.php",true);
 
             xhr.onload = function(){
                 
-                var myModal = document.getElementById('feature-s'); 
+                var myModal = document.getElementById('add-room'); 
                 var modal = bootstrap.Modal.getInstance(myModal);
                 modal.hide();
 
                 if(this.responseText == 1)
                 {
-                    alert('success','New feature added');
-                    feature_s_form.elements['feature_name'].value = '';
-                    get_features();
+                    alert('success','New room added');
+                    add_room_form.reset();
+                    
                 }
                 else{
                     alert('error,Server Down!')
                 }
             }
 
-            xhr.send(data); */
+            xhr.send(data); 
         }
 
     </script>
