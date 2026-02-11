f2df2406453824d4ce043c66400c3cdcb379a424
YashKamdar2004 <yashkamdar872@gmail.com>
2026-02-11 18:07:56 +0530

    account reset link completef

--- ajax/login_register.php
+++ ajax/login_register.php
@@ -6,19 +6,34 @@ require('../admin/inc/db_config.php');
 require('../admin/inc/essentials.php');
 require("../inc/sendgrid/sendgrid-php.php");
 
-function send_mail($uemail,$name,$token)
+date_default_timezone_set("Asia/Kolkata");
+
+function send_mail($uemail,$token,$type)
 {
+
+    if($type == "email_confirmation"){
+        $page = 'email_confirm.php';
+        $subject = "Account Verification Link";
+        $content = "confirm your email";
+    }
+    else{
+        $page = 'index.php';
+        $subject = "Account Reset Link";
+        $content = "reset your account";
+    }
+
+
     $email = new \SendGrid\Mail\Mail(); 
     $email->setFrom(SENDGRID_EMAIL,SENDGRID_NAME);
-    $email->setSubject("Account Verification Link");
+    $email->setSubject($subject);
 
-    $email->addTo($uemail,$name);
+    $email->addTo($uemail);
 
     $email->addContent(
         "text/html", 
         "
-            Click the link to confirm your email: <br>
-            <a href='".SITE_URL."email_confirm.php?email_confirmation&email=$uemail&token=$token"."'>
+            Click the link to $content: <br>
+            <a href='".SITE_URL."$page?$type&email=$uemail&token=$token"."'>
                 CLICK ME
             </a>
         
@@ -75,7 +90,7 @@ if(isset($_POST['register']))
 
     $token = bin2hex(random_bytes(16));
      
-    if(!send_mail($data['email'],$data['name'],$token))
+    if(!send_mail($data['email'],$token,"email_confirmation"))
     {
         echo 'mail_failed';
         exit;
@@ -135,7 +150,42 @@ if(isset($_POST['login']))
 
 if(isset($_POST['forgot_pass']))
 {
-        
+    $data = filteration($_POST);
+    
+     $u_exist = select("SELECT * FROM `user_cred` WHERE `email`=? LIMIT 1", [$data['email']],"s");
+
+    if(mysqli_num_rows($u_exist)==0){
+        echo 'inv_email';
+    }
+    else{
+       $u_fetch = mysqli_fetch_assoc($u_exist);
+        if($u_fetch['is_verified']==0){
+            echo 'not_verified';
+        }
+        else if($u_fetch['status']==0){
+            echo 'inactive';
+        }
+        else{
+            // send reset link to email
+            $token = bin2hex(random_bytes(16));
+
+            if(!send_mail($data['email'],$token,'account_recovery')){
+                echo 'mail_failed';
+            }
+            else{
+                $data = date("Y-m-d");
+                $query = mysqli_query($con,"UPDATE `user_cred` SET `token`=`$token`, `t_expire`=`$date` 
+                    WHERE `id`='$u_fetch[id]'");
+
+                if($query){
+                    echo 1;
+                }
+                else{
+                    echo 'upd_failed';
+                }
+            }
+        } 
+    }
 }
 
 ?>
--- inc/footer.php
+++ inc/footer.php
@@ -204,21 +204,25 @@ integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxV
     xhr.open("POST","ajax/login_register.php",true);
 
     xhr.onload = function(){
-      // if(this.responseText == 'inv_email_mob'){
-      //   alert('error',"Invalid Email or Mobile Number!");
-      // }
-      // else if(this.responseText == 'not_verified'){
-      //   alert('error',"Email is not verified!");
-      // }
-      // else if(this.responseText == 'inactive'){
-      //   alert('error',"Account Suspended! Please contact Admin...");
-      // }
-      // else if(this.responseText == 'invalid_pass'){
-      //   alert('error',"Incorrect Password!");
-      // }
-      // else{
-      //   window.location = window.location.pathname; 
-      // }
+      if(this.responseText == 'inv_email'){
+        alert('error',"Invalid Email!");
+      }
+      else if(this.responseText == 'not_verified'){
+        alert('error',"Email is not verified! Please Contact Admin");
+      }
+      else if(this.responseText == 'inactive'){
+        alert('error',"Account Suspended! Please contact Admin...");
+      }
+      else if(this.responseText == 'mail_failed'){
+        alert('error',"Cannot send email. Server Down!");
+      }
+      else if(this.responseText == 'upd_failed'){
+        alert('error',"Password reset failed. Server Down!");
+      }
+      else{
+        alert('success',"Reset link sent to email!");
+        forgot_form.reset(); 
+      }
     }
 
     xhr.send(data);
