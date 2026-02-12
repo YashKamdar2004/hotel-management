d6b8912ec9e1ed3748b94fc8714ebd91c5e604d6
YashKamdar2004 <yashkamdar872@gmail.com>
2026-02-12 20:33:39 +0530

    new file added users.php

--- room_details.php
+++ room_details.php
@@ -171,9 +171,11 @@
                 </div>
               area;
 
-              echo<<<book
-                <a href="#" class="btn w-100 text-white custom-bg shadow-none mb-1">Book Now</a> 
-              book;
+              if(!$settings_r['shutdown']){
+                echo<<<book
+                  <a href="#" class="btn w-100 text-white custom-bg shadow-none mb-1">Book Now</a> 
+                book;
+              }
 
             ?>
           </div>
